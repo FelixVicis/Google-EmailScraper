bb8a25a4cfa715a298597cfcb811c64ce682c08c
Allen Mills <Mills.Allen.James@gmail.com>
2018-09-12 11:36:19 -0700

    chore: pull license message out into file Replacing the GNU license with MIT, same overall goal.

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 '''
-Copyright 2013 Kendrick Ledet
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-Google-EmailScraper
-
 Purpose: Scraper that searches Google based on a query and scrapes all emails found on each page.
 Output files are saved as csv.
-
-Date: 5/26/13
 '''
 from search import websites
 from parse import find_emails
